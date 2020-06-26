1c6ffac1f298efc949f6bfe6b74138a0437139df
ivan-piminov <ivan-piminov@yandex.ru>
2020-06-26 10:22:04 +0300

    add NavLink and change name component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import './App.module.css';
 import Main from './UI/components/Main/Main';
 
-
 const App = () => {
     return (
         <div className="App">
             <Main/>
         </div>
     );
-}
+};
 
 export default App;
